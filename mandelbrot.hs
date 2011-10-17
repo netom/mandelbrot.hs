040255995d6e47d09eda6d929884fa088d1ad3a1
Fábián Tamás László <netom@elte.hu>
2011-10-17 22:36:11 +0200

    Added comments

--- mandelbrot.hs
+++ mandelbrot.hs
@@ -1,9 +1,16 @@
 import Graphics.GD
 import Control.Concurrent.ParallelIO
 
+-- Function for determining if a complex number is in the set
 inTheSet :: (Num a, Ord a) => (a, a) -> Int -> Bool
 inTheSet x maxiter = inTheSetW (0, 0) x maxiter
 
+-- A number is in the set if the x(n+1) = x(n)^2 + c, x(0) = 0
+-- series is bounded. A point is certainly not in the set, when
+-- it's absolute value is > 2. We check the series up to a given
+-- limit (maxiter) to see, if there is a value whose abs. is > 2.
+-- If there is, the point is not in the set, if there is none,
+-- it _probably_ is.
 inTheSetW :: (Num a, Ord a) => (a, a) -> (a, a) -> Int -> Bool
 inTheSetW (x1, x2) (c1, c2) maxiter
     | flew_away    = False
@@ -12,23 +19,30 @@ inTheSetW (x1, x2) (c1, c2) maxiter
     where
         flew_away = x1*x1+x2*x2 > 4
 
+-- Compute a rectangle part of the image. The sides are parallel to the
+-- image's sides. The rectangle is given with the upper left and lower
+-- right corners' coordinates respectively. The center is a complex
+-- number at the center of the image. The scale is a positive real
+-- number, and it's the size of one pixel on the complex plane.
 mandelbrotCU :: Image -> (Int, Int) -> (Int, Int) -> (Float, Float) -> Float -> Int -> IO ()
 mandelbrotCU image (x1, y1) (x2, y2) (xc, yc) scale maxiter = do
     mapM_ (\(x, y) -> setPixel (x, y) (if inTheSet (xp x, yp y) maxiter then 0x000000 else 0xffffff) image) [(x, y)|x <- [x1..x2], y <- [y1..y2]]
     where
-        --xp x = xc-((fromIntegral x2 + fromIntegral x1)/2-(fromIntegral x2 - fromIntegral x))*scale
-        --yp y = yc+((fromIntegral y2 + fromIntegral y1)/2-(fromIntegral y2 - fromIntegral y))*scale
           xp x = (fromIntegral x - ((fromIntegral x2 + fromIntegral x1)/2))*scale + xc
           yp y = (fromIntegral y - ((fromIntegral y2 + fromIntegral y1)/2))*scale - yc
 
+-- Auxilliary function that splits a list into well sized chunks
 chunks :: [a] -> Int -> [[a]]
 chunks list size
     | length list <= size = [list]
     | otherwise           = (take size list) : chunks (drop size list) size
 
+-- Extracts a list of (head,last) pairs from lists of lists.
 ranges :: [a] -> Int -> [(a,a)]
 ranges list size = map (\x -> (head x, last x)) (chunks list size)
 
+-- Renders a mandelbrot set onto a GD image. The center, scale and
+-- maxiter parameters are described at mandelbrotCU
 mandelbrot :: Image -> (Float, Float) -> Float -> Int -> IO ()
 mandelbrot image (cx, cy) scale maxiter = do
     (w, h) <- imageSize image
